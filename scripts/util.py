81a919eee1785e935e5f96a34c49ee369131525c
Travis Rivera <TravisGRivera@gmail.com>
2023-04-02 11:30:33 -0400

    new home for print_to_conole to remove circular dependency

---
+++ scripts/util.py
@@ -0,0 +1,31 @@
+import random
+import time
+from colorama import Style
+from config import Config
+import speak
+cfg = Config()
+
+
+def print_to_console(
+        title,
+        title_color,
+        content,
+        speak_text=False,
+        min_typing_speed=0.05,
+        max_typing_speed=0.01):
+    global cfg
+    if speak_text and cfg.speak_mode:
+        speak.say_text(f"{title}. {content}")
+    print(title_color + title + " " + Style.RESET_ALL, end="")
+    if content:
+        words = content.split()
+        for i, word in enumerate(words):
+            print(word, end="", flush=True)
+            if i < len(words) - 1:
+                print(" ", end="", flush=True)
+            typing_speed = random.uniform(min_typing_speed, max_typing_speed)
+            time.sleep(typing_speed)
+            # type faster after each word
+            min_typing_speed = min_typing_speed * 0.95
+            max_typing_speed = max_typing_speed * 0.95
+    print()
